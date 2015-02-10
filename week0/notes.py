0931480f8a59ec5ad10fc3df3bf57d7510be3be6
sunny-g <sunny.gonna@gmail.com>
2015-02-09 16:15:43 -0800

    adds week0 resources

---
+++ week0/notes.py
@@ -0,0 +1,36 @@
+'''
+################
+intro
+
+three types of work:
+    quizzes:
+        during or after videos
+        not graded
+
+    hw:
+        collection of problems, some of which are not submitted
+
+    lab:
+        like hw
+        oriented towards applications of linear algebra
+'''
+'''
+################
+comprehensions
+    produces a set or a list or a dictionary
+    gives you a bit of functional programming with lambdas
+    mimics mathematical notation
+    ** will be required for some quiz questions
+'''
+'''
+################
+the function (notes on paper)
+
+    S = {-4, 4, -3, 3, -2, 2, -1, 1, 0}
+    s = {x for x in S if x >= 0}
+    s = {0, 1, 2, 3, 4}
+
+complex numbers
+    z = 3 + 4j
+
+'''
